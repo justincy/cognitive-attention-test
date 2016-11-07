d4f0b4e7aee1b4b8ee28d7219dd1a2d24afa1891
Justin <justincyork@gmail.com>
2016-11-07 13:32:40 -0600

    Add trials and short dev mode

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
  * Configuration
  */
 
-var trials = {
-      before: [1,3,2],
-      middle: [4,3],
-      after: [8,3,6]
+var DEV = document.location.search.indexOf('dev') !== -1,
+
+    trials = {
+      before: [5,7,6,4,8,2,7,3,6,1,2,9,7,5,4,8,2,2,6,3,2,3,9,4,9,6,3,5,2,1,1,3,5,6,9,8,8,3,7,5,2,1,2,3,8,7,6,5,4,3,2,1,9,3,2,4,6,5,7,8,3,8,6,4,8,2,5,3,6,4,9,1,5,3,3,2,6,8,7,6,1,9,4,2,3,1,5,5,5,6,8,3,7,2,3,8],
+      middle: [9,2,1,1,4,6,3,8,7,8,3,6,5,7,3,9,2,6,9,1,7,3,5,4,4,5,3,7,1,9,6,2,9,3,7,5,6,3,8,7,8,3,6,4,1,1,2,9],
+      after: [8,3,2,7,3,8,6,5,5,5,1,3,2,4,9,1,6,7,8,6,2,3,3,5,1,9,4,6,3,5,2,8,4,6,8,3,8,7,5,6,4,2,3,9,1,2,3,4,5,6,7,8,3,2,1,2,5,7,3,8,8,9,6,5,3,1,1,2,5,3,6,9,4,9,3,2,3,6,2,2,8,4,5,7,9,2,1,6,3,7,2,8,4,6,7,5]
     },
 
     // Track whether we are before or after the sms notification
@@ -17,7 +19,7 @@ var trials = {
     currentTrial = 0,
 
     // Time each number is shown on the screen
-    trialTime = 2500,
+    trialTime = DEV ? 1000 : 2500,
 
     // Stats are separated into before and after the sms notification which
     // occurs half way through the test
@@ -89,7 +91,7 @@ function showTrial(){
   trialStartTime = Date.now();
 
   // Advance the state when necessary
-  if(currentTrial === trials[beforeOrAfter].length){
+  if(currentTrial === trials[beforeOrAfter].length || (DEV && currentTrial == 3)){
     if(beforeOrAfter === 'before'){
       beforeOrAfter = 'middle';
       currentTrial = 0;
